bfd9711f389b0f829ea9422803db1cf1bc2aafa0
Stefan Lindbohm <stefan@kolizz.com>
2013-04-22 00:30:20 +0200

    Search, new fields and picture handling for User

--- lib/crew_corner/user.rb
+++ lib/crew_corner/user.rb
@@ -1,7 +1,13 @@
+require 'open-uri'
+
 module CrewCorner
   class User < Base
     field :uid, :integer
     field :username
+    field :firstname
+    field :lastname
+    field :badge_picture
+    field :profile_picture
 
     def self.current(options = {})
       fetch("/1/user/get", options)
@@ -9,6 +15,30 @@ module CrewCorner
 
     def self.find(uid, options = {})
       fetch("/1/user/get/#{uid}", options)
+    end
+
+    def self.search(query, options = {})
+      fetch("/1/user/search/#{URI::encode(query)}", options) || []
+    end
+
+    def badge_picture_url(width = nil, height = nil)
+      picture_url(badge_picture, width, height)
+    end
+
+    def profile_picture_url(width = nil, height = nil)
+      picture_url(profile_picture, width, height)
+    end
+
+    private
+
+    def picture_url(base, width, height)
+      url = "http://#{base}"
+
+      params = []
+      params << "width=#{width}" unless width.nil?
+      params << "height=#{height}" unless height.nil?
+
+      url << "?#{params.join("&")}" unless params.empty?
     end
   end
 end
